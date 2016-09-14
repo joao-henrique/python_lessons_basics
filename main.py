572c8de64f2c26efe390fb9d10b1f56843958807
João Henrique <joaohenrique.p.almeida@gmail.com>
2016-09-13 22:39:34 -0300

    working arrays

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 
+from modulo import soma
+
 def main():
+    print "Modulo Soma 3 e 4 "
+    print soma(3,4)
     nome = raw_input("Digite seu nome ")
     print "Ola " + nome
 
+    a = []
+    print a
+    a.append(10)
+    a.append(100)
+    a.append(99)
+    print a
+    a.remove(10)
+    print a
 
 if __name__ == '__main__' : main()
---
+++ modulo.py
@@ -0,0 +1,7 @@
+'''
+    Funcao Para soma 2 numeros
+'''
+
+def soma (a,b):
+    c = a + b
+    return c
